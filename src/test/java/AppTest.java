66e6f2ba0ef826cea7ee562cac11479b79d03687
Dilmi Gunaratne <127681242+dilmigunaratne@users.noreply.github.com>
2025-05-23 19:41:49 +1000

    Add files via upload

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testMain() {
+        assertEquals(1, 1); // Dummy test
+    }
+}
